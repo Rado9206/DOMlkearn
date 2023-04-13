fb087e7d96d3a8e26d85fa636d1c1ccacb8e3a5c
Radek <rj.airoo@gmail.com>
2023-04-13 19:07:04 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const heading = document.querySelector('h1')
+console.log(heading);
+const allP = document.querySelectorAll('p')
+console.log(allP);
+const test = document.querySelector('.test')
+console.log(test);
+const testP = test.querySelector('#test')
+console.log(testP);
